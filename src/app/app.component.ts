0319ced4aa6b5e1fcfdf19ccdafa6dbadeceda75
Paul Bond <git@paulsbond.co.uk>
2022-04-16 22:04:15 +0100

    Moved words to a file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,16 @@
 import { Component } from '@angular/core';
-import { Word } from "./word";
+import { Word } from './word';
+import { WORDS } from './words';
 declare var HanziWriter: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  private words = [
-    new Word("的", "的", "de5", "de", "indicates possession, like adding 's to a noun"),
-    new Word("我", "我", "wo3", "wǒ", "I; me"),
-    new Word("你", "你", "ni3", "nǐ", "you (singular)"),
-  ];
-
   writer: any;
-  word: Word = this.words[0];
+  word: Word = WORDS[0];
 
   ngOnInit() {
     this.writer = new HanziWriter('writer', {
@@ -33,23 +28,42 @@ export class AppComponent {
   quiz(): void {
     this.writer.setCharacter(this.word.simplified);
     this.writer.quiz({
-        onMistake: function (strokeData: any) {
-            console.log('Oh no! you made a mistake on stroke ' + strokeData.strokeNum);
-            console.log("You've made " + strokeData.mistakesOnStroke + " mistakes on this stroke so far");
-            console.log("You've made " + strokeData.totalMistakes + " total mistakes on this quiz");
-            console.log("There are " + strokeData.strokesRemaining + " strokes remaining in this character");
-        },
-        onCorrectStroke: function (strokeData: any) {
-            console.log('Yes!!! You got stroke ' + strokeData.strokeNum + ' correct!');
-            console.log('You made ' + strokeData.mistakesOnStroke + ' mistakes on this stroke');
-            console.log("You've made " + strokeData.totalMistakes + ' total mistakes on this quiz');
-            console.log('There are ' + strokeData.strokesRemaining + ' strokes remaining in this character');
-        },
-        onComplete: function (summaryData: any) {
-            console.log('You did it! You finished drawing ' + summaryData.character);
-            console.log('You made ' + summaryData.totalMistakes + ' total mistakes on this quiz');
-        }
+      onMistake: function (strokeData: any) {
+        console.log('Oh no! you made a mistake on stroke ' + strokeData.strokeNum);
+        console.log(
+          "You've made " +
+            strokeData.mistakesOnStroke +
+            ' mistakes on this stroke so far'
+        );
+        console.log(
+          "You've made " + strokeData.totalMistakes + ' total mistakes on this quiz'
+        );
+        console.log(
+          'There are ' +
+            strokeData.strokesRemaining +
+            ' strokes remaining in this character'
+        );
+      },
+      onCorrectStroke: function (strokeData: any) {
+        console.log('Yes!!! You got stroke ' + strokeData.strokeNum + ' correct!');
+        console.log(
+          'You made ' + strokeData.mistakesOnStroke + ' mistakes on this stroke'
+        );
+        console.log(
+          "You've made " + strokeData.totalMistakes + ' total mistakes on this quiz'
+        );
+        console.log(
+          'There are ' +
+            strokeData.strokesRemaining +
+            ' strokes remaining in this character'
+        );
+      },
+      onComplete: function (summaryData: any) {
+        console.log('You did it! You finished drawing ' + summaryData.character);
+        console.log(
+          'You made ' + summaryData.totalMistakes + ' total mistakes on this quiz'
+        );
+      },
     });
-  
   }
 }
---
+++ src/app/words.ts
@@ -0,0 +1,7 @@
+import { Word } from './word';
+
+export const WORDS: Word[] = [
+  new Word('的', '的', 'de5', 'de', "indicates possession, like adding 's to a noun"),
+  new Word('我', '我', 'wo3', 'wǒ', 'I; me'),
+  new Word('你', '你', 'ni3', 'nǐ', 'you (singular)'),
+];
