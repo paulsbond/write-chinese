96832e6b33ce39d42f123290c19a7efbcd096f14
Paul Bond <git@paulsbond.co.uk>
2022-04-17 14:48:39 +0100

    Added due dates

--- src/app/word.service.ts
+++ src/app/word.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { min } from 'rxjs';
 import { Word } from './word';
 import { WORDS } from './words';
 
@@ -9,6 +10,16 @@ export class WordService {
   constructor() {}
 
   nextWord(): Word {
-    return WORDS[0];
+    let next_due_word = WORDS[0];
+    let next_due_date = WORDS[0].due;
+    if (next_due_date === undefined) return next_due_word;
+    for (let word of WORDS) {
+      if (word.due === undefined || word.due < Date.now()) return word;
+      if (word.due !== undefined && word.due < next_due_date) {
+        next_due_word = word;
+        next_due_date = word.due;
+      }
+    }
+    return next_due_word;
   }
 }
--- src/app/word.ts
+++ src/app/word.ts
@@ -1,4 +1,7 @@
 export class Word {
+  seconds: number = 30;
+  due?: number;
+
   constructor(
     public simplified: string,
     public traditional: string,
@@ -6,4 +9,14 @@ export class Word {
     public pinyin: string,
     public definition: string
   ) {}
+
+  good(): void {
+    this.due = Date.now() + this.seconds * 1000;
+    this.seconds *= 2;
+  }
+
+  again(): void {
+    this.seconds = 15;
+    this.good();
+  }
 }
